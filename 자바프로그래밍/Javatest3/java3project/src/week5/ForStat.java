026c2b2623df14970421954dbd093a8323334ab6
tg1kim <groun519@gmail.com>
2024-04-05 14:56:08 +0900

    5주차

---
+++ 자바프로그래밍/Javatest3/java3project/src/week5/ForStat.java
@@ -0,0 +1,22 @@
+package week5;
+
+public class ForStat {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1, 2, 3, 4, 5};
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		for(int i : arr) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
